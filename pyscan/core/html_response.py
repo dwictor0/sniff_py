21d7e2fe047a8833659fded268d35dd000c0b263
dwictor0 <daniel.wdev0@gmail.com>
2026-03-07 16:05:45 -0300

    FEAT: APLICANDO_CORRECAO_MODULO_HTML_RESPONSE

--- pyscan/core/html_response.py
+++ pyscan/core/html_response.py
@@ -1,3 +1,11 @@
-def generateHTML():
-    
-    
+from dataclasses import dataclass
+from typing import List
+from .html_report_host import HTMLReportHost
+from .html_report_metadata  import HTMLReportMetadata
+
+@dataclass(slots=True)
+class HTMLReport:
+    """Representa o relatorio completo
+    """
+    metadata: HTMLReportMetadata
+    hosts: List[HTMLReportHost]
